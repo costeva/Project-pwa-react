517ad2c393f84e0c2b7aa60f946205e8ffdd7ebb
Alexis Costedoat <67351701+costeva@users.noreply.github.com>
2024-08-15 08:42:16 -0300

    Configuraciones y routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homes from "./pages/Home";
+import HeroDetails from "./pages/HeroeDetail";
+import Welcome from "./pages/Welcome";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container mx-auto p-4">
+          <Nav />
+          <main>
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/home" element={<Homes />} />
+              <Route path="/people/:id" element={<HeroDetails />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
